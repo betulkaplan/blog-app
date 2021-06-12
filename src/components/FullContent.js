8a600c2432d60baa3edb7e181a5e5e1e261b84bb
betulkaplan <betulkaplan7@gmail.com>
2021-06-12 15:38:21 +0300

    edit page updated

--- src/components/FullContent.js
+++ src/components/FullContent.js
@@ -2,10 +2,25 @@ import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import { useHistory } from 'react-router-dom';
 import { deleteHandler, editHandler } from '../firebase/auth';
-import { Form, Input, TextArea, Button, Image } from 'semantic-ui-react';
+import { Form, Input, TextArea, Image } from 'semantic-ui-react';
 import { Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
+import EditIcon from '@material-ui/icons/Edit';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
 
 const FullContent = (props) => {
+  const classes = useStyles();
   console.log(props.location.post);
   const post = props.location.post;
   const history = useHistory();
@@ -15,6 +30,8 @@ const FullContent = (props) => {
   const [title, setTitle] = useState(post?.title);
   const [url, setUrl] = useState(post?.url);
   const [text, setText] = useState(post?.text);
+
+  const [isEditting, setIsEditting] = useState(false);
 
   useEffect(() => {
     console.log(post?.id);
@@ -26,9 +43,11 @@ const FullContent = (props) => {
   }, []);
   return (
     <div>
+      <img src={post.url} alt="" />
       {edit ? (
         <Form
           onSubmit={() => {
+            setIsEditting(false);
             editHandler(post);
             history.push('/');
           }}
@@ -63,11 +82,16 @@ const FullContent = (props) => {
               post.text = e.target.value;
             }}
           />
-          <Form.Field
-            id="form-button-control-public"
-            control={Button}
-            content="Submit"
-          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            size="small"
+            className={classes.button}
+            startIcon={<SaveIcon />}
+          >
+            Save
+          </Button>
         </Form>
       ) : null}
       {!currentUser ? null : (
@@ -77,22 +101,36 @@ const FullContent = (props) => {
           <h3>{post?.text}</h3>
           {currentUser.email === post.author ? (
             <>
-              <button
-                onClick={() => {
-                  deleteHandler(post?.id);
-                  history.push('/');
-                }}
-              >
-                Delete
-              </button>
-              <button
-                onClick={() => {
-                  setEdit(!edit);
-                  console.log('edit');
-                }}
-              >
-                Edit
-              </button>
+              {!isEditting ? (
+                <>
+                  <Button
+                    onClick={() => {
+                      deleteHandler(post?.id);
+                      history.push('/');
+                    }}
+                    variant="contained"
+                    color="secondary"
+                    className={classes.button}
+                    startIcon={<DeleteIcon />}
+                  >
+                    Delete
+                  </Button>
+
+                  <Button
+                    onClick={() => {
+                      setIsEditting(true);
+                      setEdit(!edit);
+                      console.log('edit');
+                    }}
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                    startIcon={<EditIcon />}
+                  >
+                    Edit
+                  </Button>
+                </>
+              ) : null}
             </>
           ) : null}
         </>
